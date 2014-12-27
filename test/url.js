df920094ce78dae317bceca0b9a88fe0dfe773e5
yelo <zhihuzeye@gmail.com>
2014-12-28 04:43:01 +0800

    restruct test

--- test/url.js
+++ test/url.js
@@ -2,25 +2,32 @@ var expect = require('chai').expect;
 
 var Request = require('./assets/request');
 var hosts = require('../');
-var config = require('fs').readFileSync(require('path').join(__dirname, './assets/hosts')).toString();
 
+var fs = require('fs');
+var path = require('path');
 
+var load = function (filename) {
+  return fs.readFileSync(path.join(__dirname, './assets/' + filename)).toString();
+}
 
 describe('url', function () {
   it('http://not-match.com', function () {
     var request = new Request('http://no-match.com');
+    var config = load('hosts');
     hosts(config)(request);
     expect(request.url).to.be.equal('http://no-match.com');
     expect(request.headers).to.be.deep.equal({});
   });
   it('http://match.com', function () {
     var request = new Request('http://match.com');
+    var config = load('hosts');
     request = hosts(config)(request);
     expect(request.url).to.be.equal('http://192.168.10.12:80/');
     expect(request.headers).to.be.deep.equal({Host: 'match.com'});
   });
   it('https://match.com:8080/path/to?search=foobar#hash', function () {
     var request = new Request('https://match.com:8080/path/to?search=foobar#hash');
+    var config = load('hosts');
     request = hosts(config)(request);
     expect(request.url).to.be.equal('https://192.168.10.12:8080/path/to?search=foobar#hash');
     expect(request.headers).to.be.deep.equal({Host: 'match.com'});
