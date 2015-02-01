e00f32dae5eeed07a80d2559e86ff40edc948984
yelo <zhihuzeye@gmail.com>
2015-02-01 20:46:32 +0800

    use hosts-parser module

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
 var url = require('url');
-
-function parser (hosts) {
-  var parsed = [];
-  hosts = (hosts || '').split('\n');
-  hosts.forEach(function (line) {
-    var match;
-    line = line.trim();
-    if (line[0] === '#') {
-      return;
-    }
-    match = line.match(/([^\s]*)(?:\s*)([^\s]*)/);
-    if (match && match.length === 3) {
-      parsed.push({
-        domain: match[1],
-        cname: match[2]
-      })
-    }
-  });
-  return parsed;
-}
-
-function match (hosts, domain) {
-  hosts.forEach(function (group) {
-    if (group.domain === domain) {
-      domain = group.cname;
-    }
-  });
-  return domain;
-}
+var Hosts = require('hosts-parser');
 
 module.exports = function (hosts) {
-  hosts = parser(hosts);
+  hosts = new Hosts(hosts);
   return function (request) {
     var uri = url.parse(request.url);
-    var cname = match(hosts, uri.hostname);
+    var ip = hosts.resolve(uri.hostname);
 
-    if (cname !== uri.hostname) {
-      request.url = uri.protocol + '//' + cname + ':' + (uri.port || '80') + (uri.path || '') + (uri.hash || '');
+    if (ip) {
+      request.url = uri.protocol + '//' + ip + ':' + (uri.port || '80') + (uri.path || '') + (uri.hash || '');
       request.set('Host', uri.hostname);
     }
 
